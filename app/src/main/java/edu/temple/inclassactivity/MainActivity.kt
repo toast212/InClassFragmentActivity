f35f8b8d0d31cda5c5010064e87ad439b006e06f
toast212 <156926783+toast212@users.noreply.github.com>
2025-03-13 11:58:45 -0400

    Attach instance of ImageDisplayFragment using factory method

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         typedArray.recycle()
 
         // Attach an instance of ImageDisplayFragment using factory method
+        supportFragmentManager.beginTransaction().replace(android.R.id.content, ImageDisplayFragment.newInstance(imageArray)).commit()
 
     }
 }
